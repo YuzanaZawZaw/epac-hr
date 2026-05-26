fe1b17e5e9407f3b0cd714cd1b188780595b0982
Yuzana Zaw Zaw (Yuzi) <70750475+YuzanaZawZaw@users.noreply.github.com>
2026-05-26 10:42:57 +1000

    Add application configuration and pom.xml

---
+++ src/main/java/com/epac/hr/EpacHrApplication.java
@@ -0,0 +1,12 @@
+package com.epac.hr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpacHrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EpacHrApplication.class, args);
+    }
+}
